bd95f4bd48e659d6179602c9d3b8aadbd7af15ad
João Santos <joaomarcelo.ms@gmail.com>
2025-06-14 11:16:13 -0300

    first commit

---
+++ app/src/main/java/com/jomar/speechtotext/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jomar.speechtotext
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.jomar.speechtotext.ui.theme.SpeechToTextTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SpeechToTextTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SpeechToTextRoot()
+                }
+            }
+        }
+    }
+}
